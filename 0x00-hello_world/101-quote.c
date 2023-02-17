72ad1adb69ffac8de6c05dd6b4cc8c8756ed4e29
Tokinipepple <Tamunotokiniomubopepple@gmail.com>
2023-02-17 02:13:24 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that print extactly
+ * main - a program that print extactly followed by a new line, to the standard error.
  * Return: 1
  */
 int main(void)
